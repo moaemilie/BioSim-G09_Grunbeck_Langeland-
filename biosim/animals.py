39cda0a84f40bd756b0e601a63f95e4e6e5e7304
Lina Grünbeck <lina.bodil.grunbeck@nmbu.no>
2021-01-05 13:30:59 +0100

    Added Animals class and Herbivore subclass.

--- biosim/animals.py
+++ biosim/animals.py
@@ -3,5 +3,55 @@
 
 """
 
-__author__ = 'Lina Grünbeck / NMBU'
-__email__ = 'lina.bodil.grunbeck@nmbu.no'
+__author__ = 'Emilie Giltvedt Langeland & Lina Grünbeck / NMBU'
+
+
+
+class Animals:
+
+    animal_count = 0
+
+
+    @classmethod
+    def count_new_animal(cls):
+        cls.animal_count += 1
+
+    @classmethod
+    def num_animals(cls):
+        return cls.animal_count
+
+    def __init__(self, age, weight, species):
+        self.age = age
+        self.weight = weight
+        self.species = species
+
+        if self.species == 'Herbivore' or 'herbivore':
+            class Herbivore(Animals):
+
+                def __init__(self, age, weight):
+                    self.age = age
+                    self.weight = weight
+                    self.w_birth = 8
+
+
+                def fitness(self):
+
+
+
+                def feeding(self):
+
+
+
+                def birth(self):
+
+
+
+
+
+
+
+
+
+
+
+
