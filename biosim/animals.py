94afcf6f681d4f4b41f99de3fedf7b5467cc4d9b
emilie <emilie.giltvedt.langeland@nmbu.no>
2021-01-06 13:17:07 +0100

    Changed animal classes.

--- biosim/animals.py
+++ biosim/animals.py
@@ -11,14 +11,6 @@ import math
 class Animals:
     animal_count = 0
 
-    @classmethod
-    def count_new_animal(cls):
-        cls.animal_count += 1
-
-
-    @classmethod
-    def num_animals(cls):
-        return cls.animal_count
 
     def __init__(self, age, weight, species):
         self.age = age
@@ -26,40 +18,46 @@ class Animals:
         self.species = species
         self.count_new_animal()
 
-        if self.species == 'Herbivore' or 'herbivore':
-            class Herbivore(Animals):
 
-                def __init__(self, age, weight):
-                    self.age = age
-                    self.weight = weight
-                    self.w_birth = 8
-                    self.sigma_birth = 1.5
-                    self.beta = 0.9
-                    self.eta = 0.05
-                    self.a_half = 40
-                    self.phi_age = 0.6
-                    self.w_half = 10
-                    self.phi_weight = 0.1
-                    self.mu = 0.25
-                    self.gamma = 0.2
-                    self.zeta = 3.5
-                    self.xi = 1.2
-                    self.omega = 0.4
-                    self.F = 10
-                    self.DeltaPhiMax = None
 
-                    self.fit = 0
+class Herbivore(Animals):
+    def __init__(self, age, weight, species):
+        super().__init__(age, weight, species)
 
-                def fitness(self):
-                    if weight <= 0:
-                        self.fit = 0
-                    else:
-                        self.fit = (1/(1+math.exp(self.phi_age*(self.age-self.a_half)))) * (1/(1+math.exp(-self.phi_weight*(self.weight-self.w_half))))
-                        return self.fit
 
-                def feeding(self):
-                    self.weight
+    def __init__(self, age, weight):
+        self.age = age
+        self.weight = weight
+        self.w_birth = 8
+        self.sigma_birth = 1.5
+        self.beta = 0.9
+        self.eta = 0.05
+        self.a_half = 40
+        self.phi_age = 0.6
+        self.w_half = 10
+        self.phi_weight = 0.1
+        self.mu = 0.25
+        self.gamma = 0.2
+        self.zeta = 3.5
+        self.xi = 1.2
+        self.omega = 0.4
+        self.F = 10
+        self.DeltaPhiMax = None
 
-                def birth(self):
+        self.fit = 0
+
+    def fitness(self):
+        if self.weight <= 0:
+            self.fit = 0
+        else:
+            self.fit = (1/(1+math.exp(self.phi_age*(self.age-self.a_half)))) * (1/(1+math.exp(-self.phi_weight*(self.weight-self.w_half))))
+            return self.fit
+
+    #def feeding(self):
+        #self.weight
+
+    #def birth(self):
+
+
 
 
--- tests/test_animals.py
+++ tests/test_animals.py
@@ -1,4 +1,15 @@
 
 from biosim.animals import Animals
 
+class testAnimals:
+
+    def test_animal_count(self, Animal):
+        """
+        Tests if the animal counter counts correct.
+        """
+
+        # set all members to their initial value
+
+        animals = [Animals(1, 5, 'Herbivore'), Animals(1,2,'Herbivore')]
+        assert Animals.num_animals() == len(animals)
 
