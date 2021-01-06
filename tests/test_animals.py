cbc9140920e40c51e3181fa1d0d3c12a535891de
emilie <emilie.giltvedt.langeland@nmbu.no>
2021-01-06 11:03:50 +0100

    Added a test file that test the class Animals.

---
+++ tests/test_animals.py
@@ -0,0 +1,13 @@
+
+from biosim.animals import Animals
+
+
+def test_animal_count():
+    """
+    Tests if the animal counter counts correct.
+    """
+
+    # set all members to their initial value
+
+
+    assert
