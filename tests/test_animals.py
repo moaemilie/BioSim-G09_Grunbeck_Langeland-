808e95f650554fddba00d349baaa5c901bbd701f
emilie <emilie.giltvedt.langeland@nmbu.no>
2021-01-07 13:05:25 +0100

    Corrected some tests.

--- tests/test_animals.py
+++ tests/test_animals.py
@@ -6,7 +6,7 @@ def test_animal_aging():
     Tests that the aging function counts correctly
     """
     years = 4
-    sheep = Herbivore(0,2,'Herbivore')
+    sheep = Herbivore(0,2)
 
     for _ in range(years):
         sheep.aging()
@@ -17,11 +17,18 @@ def test_animal_fitness():
     """
     Tests that the fitness always lies between 0 and 1
     """
-    sheep = Herbivore(4,30,'Herbivore')
+    sheep = Herbivore(4,30)
     assert sheep.fitness() >= 0 and sheep.fitness() <= 1
 
 
-#def test_weight_loss():
+def test_weghtloss():
+    sheep = Herbivore(4, 30)
+    weight_delta = 30 * sheep.eta
+    animal_weight = 30
+    assert sheep.weightloss() == animal_weight - weight_delta
+
+
+#def test_weight_loss_death():
     #sheep = Herbivore(0, 30, 'Herbivore')
     #sheep.weightloss()
     #assert sheep.death() == True
