d7c5e3dd1ce50b3bdc9757afd749b97f2b35350e
emilie <emilie.giltvedt.langeland@nmbu.no>
2021-01-07 11:00:56 +0100

    Added test for fitness.

--- tests/test_animals.py
+++ tests/test_animals.py
@@ -2,6 +2,9 @@
 from biosim.animals import Animals
 
 def test_animal_aging():
+    """
+    Tests that the aging function counts correctly
+    """
     years = 4
     Zebra = Animals(0,2,'Herbivore')
 
@@ -9,3 +12,8 @@ def test_animal_aging():
         Zebra.aging()
     assert Zebra.age == 4
 
+
+def test_animal_fitnes():
+
+    Lion = Animals(4,30,'Herbivore')
+    assert Lion.fitness() < 0 and Lion.fitness() > 1
