7210ee5c372c15973b61eec4d6d68ec5dd63b324
emilie <emilie.giltvedt.langeland@nmbu.no>
2021-01-10 14:48:08 +0100

    Deleted the default params in self so you only have them in a dictionary.

--- biosim/animals.py
+++ biosim/animals.py
@@ -12,25 +12,10 @@ random.seed(123456)
 
 
 class Animals:
-    w_birth = None
-    sigma_birth = None
-    beta = None
-    eta = None
-    a_half = None
-    phi_age = None
-    w_half = None
-    phi_weight = None
-    mu = None
-    gamma = None
-    zeta = None
-    xi = None
-    omega = None
-    F = None
-    DeltaPhiMax = None
 
-    default_params = {'w_birth': w_birth, 'sigma_birth': sigma_birth, 'beta': beta, 'eta': eta, 'a_half': a_half,
-                      'phi_age': phi_age, 'w_half': w_half, 'phi_weight': phi_weight, 'mu': mu, 'gamma': gamma,
-                      'zeta': zeta, 'xi': xi, 'omega': omega, 'F': F, 'DeltaPhiMax': DeltaPhiMax}
+    default_params = {'w_birth': None, 'sigma_birth': None, 'beta': None, 'eta': None, 'a_half': None,
+                      'phi_age': None, 'w_half': None, 'phi_weight': None, 'mu': None, 'gamma': None,
+                      'zeta': None, 'xi': None, 'omega': None, 'F': None, 'DeltaPhiMax': None}
 
     @classmethod
     def set_params(cls, new_params):
@@ -58,6 +43,7 @@ class Animals:
         self.age = age
         self.weight = weight
         self.fit = self.fitness()
+        self.babyweight = None
 
     def fitness(self):
         """
@@ -71,7 +57,7 @@ class Animals:
         if self.weight <= 0:
             self.fit = 0
         else:
-            self.fit = (1 / (1 + math.exp(self.phi_age * (self.age - self.a_half)))) * (1 / (1 + math.exp((-self.phi_weight) * (self.weight - self.w_half))))
+            self.fit = (1 / (1 + math.exp(self.default_params["phi_age"] * (self.age - self.default_params["a_half"]))) * (1 / (1 + math.exp((-self.default_params["phi_weight"]) * (self.weight - self.default_params["w_half"])))))
         return self.fit
 
     def aging(self):
@@ -95,7 +81,7 @@ class Animals:
         Int
             with weight for the animal
         """
-        self.weight -= self.weight * self.eta
+        self.weight -= self.weight * self.default_params["eta"]
         return self.weight
 
     def death(self):
@@ -110,7 +96,7 @@ class Animals:
         if self.weight <= 0:
             p_death = 1.0
         else:
-            p_death = (self.omega*(1-self.fit))
+            p_death = (self.default_params["omega"]*(1-self.fit))
         return random.random() <= p_death
 
     def eating(self, F_line):
@@ -127,7 +113,7 @@ class Animals:
         int
             New weight of animal
         """
-        self.weight += self.beta * F_line
+        self.weight += self.default_params["beta"] * F_line
         return self.weight
 
     def birth(self, N):
@@ -144,63 +130,35 @@ class Animals:
         Bool
                 True if animal should be added.
         """
-        baby_weight = random.gauss(self.w_birth, self.sigma_birth)
+        baby_weight = random.gauss(self.default_params["w_birth"], self.default_params["sigma_birth"])
 
-        p_birth = min(1, self.gamma * self.fit * (N - 1))
+        p_birth = min(1, self.default_params["gamma"] * self.fit * (N - 1))
 
-        if self.weight < self.zeta*(self.w_birth + self.sigma_birth) or self.weight < baby_weight:
+        if self.weight < self.default_params["zeta"]*(self.default_params["w_birth"] + self.default_params["sigma_birth"]) or self.weight < baby_weight:
             p_birth = 0
         chance = random.random() <= p_birth
         if chance == True:
-            self.weight -= baby_weight * self.xi
-        return chance, baby_weight
+            self.weight -= baby_weight * self.default_params["xi"]
+            self.babyweight = baby_weight
+        return chance
 
 
 class Herbivore(Animals):
-    w_birth = 8
-    sigma_birth = 1.5
-    beta = 0.9
-    eta = 0.05
-    a_half = 40
-    phi_age = 0.6
-    w_half = 10
-    phi_weight = 0.1
-    mu = 0.25
-    gamma = 0.2
-    zeta = 3.5
-    xi = 1.2
-    omega = 0.4
-    F = 10
-    DeltaPhiMax = None
 
-    default_params = {'w_birth': w_birth, 'sigma_birth': sigma_birth, 'beta': beta, 'eta': eta, 'a_half': a_half,
-                      'phi_age': phi_age, 'w_half': w_half, 'phi_weight': phi_weight, 'mu': mu, 'gamma': gamma,
-                      'zeta': zeta, 'xi': xi, 'omega': omega, 'F': F, 'DeltaPhiMax': DeltaPhiMax}
+    default_params = {'w_birth': 8, 'sigma_birth': 1.5, 'beta': 0.9, 'eta': 0.05, 'a_half': 40,
+                      'phi_age': 0.6, 'w_half': 10, 'phi_weight': 0.1, 'mu': 0.25, 'gamma': 0.2,
+                      'zeta': 3.5, 'xi': 1.2, 'omega': 0.4, 'F': 10, 'DeltaPhiMax': None}
 
     def __init__(self, age, weight):
         super().__init__(age, weight)
 
 
 class Carnivore(Animals):
-    w_birth = 6
-    sigma_birth = 1
-    beta = 0.75
-    eta = 0.125
-    a_half = 40
-    phi_age = 0.3
-    w_half = 4
-    phi_weight = 0.4
-    mu = 0.4
-    gamma = 0.8
-    zeta = 3.5
-    xi = 1.1
-    omega = 0.8
-    F = 50
-    DeltaPhiMax = 10
 
-    default_params = {'w_birth': w_birth, 'sigma_birth': sigma_birth, 'beta': beta, 'eta': eta, 'a_half': a_half,
-                      'phi_age': phi_age, 'w_half': w_half, 'phi_weight': phi_weight, 'mu': mu, 'gamma': gamma,
-                      'zeta': zeta, 'xi': xi, 'omega': omega, 'F': F, 'DeltaPhiMax': DeltaPhiMax}
+    default_params = {'w_birth': 6, 'sigma_birth': 1, 'beta': 0.75, 'eta': 0.125, 'a_half': 40,
+                      'phi_age': 0.3, 'w_half': 4, 'phi_weight': 0.4, 'mu': 0.4, 'gamma': 0.8,
+                      'zeta': 3.5, 'xi': 1.1, 'omega': 0.8, 'F': 50, 'DeltaPhiMax': 10}
+
 
     def __init__(self, age, weight):
         super().__init__(age, weight)
