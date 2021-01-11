db60c773b988325a09d8fad693f26c63dd63e37c
Lina Grünbeck <lina.bodil.grunbeck@nmbu.no>
2021-01-11 11:34:50 +0100

    Updated feeding method in cell class.

--- biosim/cell.py
+++ biosim/cell.py
@@ -75,10 +75,43 @@ class Cell:
         return self.herb_pop.extend(newborns(self.herb_pop))
 
     def feeding(self):
+        """
+        Feeds herbivores and carnivores in a cell.
 
-        # F = self.f_max
+        Returns
+        -------
+        list
+            Updated populations
+        """
+
+        self.set_f_max(self.default_f_max['f_max'])
+
         for herb in self.herb_pop:
-            herb.eating(8)
+            if self.default_f_max['f_max'] >= herb.default_params["F"]:
+                herb.eating(herb.default_params["F"])
+                self.default_f_max['f_max'] -= herb.default_params["F"]
+            elif self.default_f_max['f_max'] < herb.default_params["F"] and self.default_f_max['f_max'] != 0:
+                herb.fodder = self.default_f_max['f_max']
+                herb.eating(self.default_f_max['f_max'])
+            herb.fitness()
+
+            def sort_pop(pop, reverse=False):
+                for j in reversed(range(len(pop))):
+                    for k in range(j):
+                        if self.herb_pop[k + 1].fit < self.herb_pop[k].fit and reverse == False:
+                            self.herb_pop[k], self.herb_pop[k + 1] = self.herb_pop[k + 1], self.herb_pop[k]
+                        elif self.herb_pop[k + 1].fit < self.herb_pop[k].fit and reverse == True:
+                            self.herb_pop[k], self.herb_pop[k + 1] = self.herb_pop[k + 1], self.herb_pop[k]
+                return pop
+
+        self.herb_pop = sort_pop(self.herb_pop)
+        self.carn_pop = sort_pop(self.carn_pop, reverse=True)
+
+        for carn in self.carn_pop:
+            for herb in self.herb_pop:
+                if carn.killing() == True:
+                    carn.eating(herb.weight)
+                    carn.fitness()
 
     def fitness(self):
         """
