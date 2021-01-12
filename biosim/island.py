afa93723ac62f37c6767748fd20e6edcb4b3b8b9
Lina Grünbeck <lina.bodil.grunbeck@nmbu.no>
2021-01-12 11:32:59 +0100

    Added more to make_map.

--- biosim/island.py
+++ biosim/island.py
@@ -5,11 +5,15 @@
 
 __author__ = 'Emilie Giltvedt Langeland & Lina Grünbeck / NMBU'
 
+from biosim.landscape import Lowland
+from biosim.landscape import Highland
+from biosim.landscape import Desert
+from biosim.landscape import Water
+
 class Island():
 
     def __init__(self, island_map):
         self.island_map = island_map
-        cells = []
 
     def make_map(self):
         self.island_map = self.island_map.split('\n')
@@ -18,7 +22,13 @@ class Island():
         for string in self.island_map:
             for letter in string:
                 if letter != 'W' or 'H' or 'L' or'D':
-                    raise KeyError ('Invalid landscape letter: ' + letter)
+                    raise KeyError('Invalid landscape letter: ' + letter)
+
+        landscapes = {'W': Water(0, 0), 'L': Lowland(0, 0), 'H': Highland(0, 0), 'D': Desert(0, 0)}
+
+        for line in self.island_map:
+            for string, num in zip(line, range(len(line))):
+                line[num] = landscapes[string]
 
         return self.island_map
 
