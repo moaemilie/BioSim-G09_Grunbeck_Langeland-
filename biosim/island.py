be7ebe9fe66b2d77f56218dc961869fa99859b49
emilie <emilie.giltvedt.langeland@nmbu.no>
2021-01-12 13:53:52 +0100

    Made smaler functions instead of one big functin, for the function make map.

--- biosim/island.py
+++ biosim/island.py
@@ -23,27 +23,37 @@ class Island():
 
         for string in self.island_map:
             for letter in string:
-                if letter not in ('W', 'H', 'L', 'D'):
+                if letter not in ('W', 'H', 'L', 'D', ' '):
                     raise ValueError('Invalid landscape letter: ' + letter)
 
         if self.island_map[0][0:-1] or self.island_map[-1][0:-1] or self.island_map[0:-1][0] or self.island_map[0:-1][-1] != 'W':
             return ValueError('Map must be surrounded by water')
 
+    def add_classes(self):
         landscapes = {'W': Water(0, 0), 'L': Lowland(0, 0), 'H': Highland(0, 0), 'D': Desert(0, 0)}
 
+        new_list = []
         for line in self.island_map:
-            for string, num in zip(line, range(len(line))):
-                line[num] = landscapes[string]
-
-        self.map_rows = len(self.island_map[0])
-        self.map_columns = len(self.island_map)
-
+            for string in line:
+                new_list.append(landscapes[string])
+                new_list = self.island_map
         return self.island_map
 
 
+    def find_num_col_row(self):
+        self.map_rows = len(self.island_map)
+        self.map_columns = len(self.island_map[0])
+        return self.map_rows, self.map_columns
+
+
     def add_animals(self, coordinates, num_herb, num_carn):
-        if coordinates[0] > self.map_rows or coordinates[1] > self.map_columns:
+        x_coor = coordinates[0]
+        y_coor = coordinates[1]
+        if y_coor > self.map_rows or x_coor > self.map_columns:
             return ValueError('Coordinate out of range' + coordinates)
 
+        origin = self.island_map[x_coor - 1][y_coor - 1]
+        origin.num_herb = num_herb
+        origin.num_carn = num_carn
 
     #def move(self):
