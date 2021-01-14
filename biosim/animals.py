0a3ec09cef7b81e806a5619a39038b4ace328f46
Lina Grünbeck <lina.bodil.grunbeck@nmbu.no>
2021-01-14 14:16:08 +0100

    Reorganized fit function on line 60-64. Changed input in birth from N to n. Changed init in subclasses from (age, weight) to (animal_info).

--- biosim/animals.py
+++ biosim/animals.py
@@ -7,12 +7,11 @@ __author__ = 'Emilie Giltvedt Langeland & Lina Grünbeck / NMBU'
 
 import math
 import random
+
 random.seed(123456)
 
 
-
 class Animals:
-
     default_params = {'w_birth': None, 'sigma_birth': None, 'beta': None, 'eta': None, 'a_half': None,
                       'phi_age': None, 'w_half': None, 'phi_weight': None, 'mu': None, 'gamma': None,
                       'zeta': None, 'xi': None, 'omega': None, 'F': None, 'DeltaPhiMax': None}
@@ -58,7 +57,10 @@ class Animals:
         if self.weight <= 0:
             self.fit = 0
         else:
-            self.fit = (1 / (1 + math.exp(self.default_params["phi_age"] * (self.age - self.default_params["a_half"]))) * (1 / (1 + math.exp((-self.default_params["phi_weight"]) * (self.weight - self.default_params["w_half"])))))
+            self.fit = (1 / (
+                        1 + math.exp(self.default_params["phi_age"] * (self.age - self.default_params["a_half"]))) *
+                        (1 / (1 + math.exp((-self.default_params["phi_weight"]) *
+                                           (self.weight - self.default_params["w_half"])))))
         return self.fit
 
     def aging(self):
@@ -97,7 +99,7 @@ class Animals:
         if self.weight <= 0:
             p_death = 1.0
         else:
-            p_death = (self.default_params["omega"]*(1-self.fit))
+            p_death = (self.default_params["omega"] * (1 - self.fit))
         return random.random() <= p_death
 
     def eating(self, fodder):
@@ -106,7 +108,7 @@ class Animals:
 
         Parameters
         ----------
-        F_line: int
+        fodder: int
                 with the amount of food consumed by an animal.
 
         Returns
@@ -117,7 +119,7 @@ class Animals:
         self.weight += self.default_params["beta"] * fodder
         return self.weight
 
-    def birth(self, N):
+    def birth(self, n):
         """
         Decides if a new animal should be added to the simulation.
 
@@ -133,32 +135,34 @@ class Animals:
         """
         baby_weight = random.gauss(self.default_params["w_birth"], self.default_params["sigma_birth"])
 
-        p_birth = min(1, self.default_params["gamma"] * self.fit * (N - 1))
+        p_birth = min(1, self.default_params["gamma"] * self.fit * (n - 1))
 
-        if self.weight < self.default_params["zeta"]*(self.default_params["w_birth"] + self.default_params["sigma_birth"]) or self.weight < baby_weight:
+        if self.weight < self.default_params["zeta"] * (
+                self.default_params["w_birth"] + self.default_params["sigma_birth"]) or self.weight < baby_weight:
             p_birth = 0
         chance = random.random() <= p_birth
-        if chance == True:
+        if chance:
             self.weight -= baby_weight * self.default_params["xi"]
             self.babyweight = baby_weight
         return chance
 
 
 class Herbivore(Animals):
-
     default_params = {'w_birth': 8, 'sigma_birth': 1.5, 'beta': 0.9, 'eta': 0.05, 'a_half': 40,
                       'phi_age': 0.6, 'w_half': 10, 'phi_weight': 0.1, 'mu': 0.25, 'gamma': 0.2,
                       'zeta': 3.5, 'xi': 1.2, 'omega': 0.4, 'F': 10, 'DeltaPhiMax': None}
 
-    def __init__(self, age, weight):
-        super().__init__(age, weight)
+    def __init__(self, animal_info):
+        super().__init__(animal_info)
 
 
 class Carnivore(Animals):
-
     default_params = {'w_birth': 6, 'sigma_birth': 1, 'beta': 0.75, 'eta': 0.125, 'a_half': 40,
                       'phi_age': 0.3, 'w_half': 4, 'phi_weight': 0.4, 'mu': 0.4, 'gamma': 0.8,
                       'zeta': 3.5, 'xi': 1.1, 'omega': 0.8, 'F': 50, 'DeltaPhiMax': 10}
+
+    def __init__(self, animal_info):
+        super().__init__(animal_info)
 
     def kill(self, fit_herb):
         """
@@ -177,11 +181,7 @@ class Carnivore(Animals):
         if fit_herb >= self.fit or self.weight <= 0:
             p_kill = 0
         elif 0 < self.fit - fit_herb < self.default_params["DeltaPhiMax"]:
-            p_kill = (self.fit - fit_herb)/(self.default_params["DeltaPhiMax"])
+            p_kill = (self.fit - fit_herb) / (self.default_params["DeltaPhiMax"])
         else:
             p_kill = 1
         return random.random() <= p_kill
-
-    def __init__(self, age, weight):
-        super().__init__(age, weight)
-
