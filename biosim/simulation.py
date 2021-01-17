f10256c0aff478a667974aa818c9825230906fb9
Lina Grünbeck <lina.bodil.grunbeck@nmbu.no>
2021-01-17 22:48:52 +0100

    Added property year and added self.sim_year. Changed variable names of set_animal_parameters and set_landscape_parameters. Added more self values.

--- biosim/simulation.py
+++ biosim/simulation.py
@@ -14,8 +14,16 @@ import random
 
 
 class BioSim:
-    def __init__(self, island_map, ini_pop, seed):
+    def __init__(self, island_map, ini_pop, seed,
+                 ymax_animals=None, cmax_animals=None, hist_specs=None,
+                 img_base=None, img_fmt='png'):
         random.seed(seed)
+        self.sim_year = 0
+        self.ymax_animals = ymax_animals
+        self.cmax_animals = cmax_animals
+        self.hist_specs = hist_specs
+        self.img_base = img_base
+        self.img_fmt = img_fmt
         self.sim_island = Island(island_map)
         self.sim_island.make_map()
         if ini_pop != []:
@@ -24,17 +32,19 @@ class BioSim:
             else:
                 self.sim_island.add_animals_island(ini_pop[0]['loc'], None, ini_pop[0]['pop'])
 
-    @staticmethod
-    def set_landscape_parameters(land_type, new_f_max):
-        Island.set_landscape_parameters(land_type,new_f_max)
+    def set_animal_parameters(self, species, params):
+        """
+        Set parameters for animal species.
+        """
+        self.sim_island.set_animal_parameters(species, params)
 
+    def set_landscape_parameters(self, landscape, params):
+        """
+        Set parameters for landscape type.
+        """
+        self.sim_island.set_landscape_parameters(landscape, params)
 
-    @staticmethod
-    def set_animal_parameters(animal_type, new_params):
-        Island.set_animal_parameters(animal_type, new_params)
-
-
-    def simulate(self, num_years):
+    def simulate(self, num_years, vis_years=1, img_years=None):
         def simulate_year():
             self.sim_island.aging_island()
             self.sim_island.weightloss_island()
@@ -44,16 +54,24 @@ class BioSim:
             self.sim_island.death_island()
             self.sim_island.move_island()
             self.sim_island.add_immigrants_island()
+            self.sim_year += 1
             print(self.sim_island.get_num_herb(), self.sim_island.get_num_carn())
+
         for year in range(num_years):
             simulate_year()
-
 
     def add_population(self, population):
         if population[0]['pop'][0]['species'] == 'Herbivore':
             self.sim_island.add_animals_island(population[0]['loc'], population[0]['pop'], None)
         else:
             self.sim_island.add_animals_island(population[0]['loc'], None, population[0]['pop'])
+
+    @property
+    def year(self):
+        """
+        Last year simulated.
+        """
+        return self.sim_year
 
     @property
     def num_animals(self):
