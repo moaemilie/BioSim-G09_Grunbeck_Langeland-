c98c7d2e6322ae49274ee5d2ce354c029db5c86a
Lina Grünbeck <lina.bodil.grunbeck@nmbu.no>
2021-01-18 16:47:44 +0100

    Added hist_plot.

--- biosim/graphics.py
+++ biosim/graphics.py
@@ -11,16 +11,20 @@ import numpy as np
 
 
 class Graphics:
-    def __init__(self, ymax_animals=None, cmax_animals=None):
+    def __init__(self, ymax_animals=None, cmax_animals=None, hist_specs=None):
         self.ymax_animals = ymax_animals
         self.cmax_animals = cmax_animals
+        self.hist_specs = hist_specs
 
         self.fig = plt.figure()
-        self.ax1 = self.fig.add_subplot(2, 3, 1)
-        self.ax_line = self.fig.add_subplot(2, 3, 3)
-        self.ax3 = self.fig.add_subplot(2, 3, 4)
-        self.ax4 = self.fig.add_subplot(2, 3, 5)
-        self.ax5 = self.fig.add_subplot(2, 3, 6)
+        self.ax_map = self.fig.add_subplot(3, 3, 1)
+        self.ax_line = self.fig.add_subplot(3, 3, 3)
+        self.ax3 = self.fig.add_subplot(3, 3, 4)
+        self.ax4 = self.fig.add_subplot(3, 3, 5)
+        self.ax5 = self.fig.add_subplot(3, 3, 6)
+        self.ax_hist1 = self.fig.add_subplot(3, 3, 7)
+        self.ax_hist2 = self.fig.add_subplot(3, 3, 8)
+        self.ax_hist3 = self.fig.add_subplot(3, 3, 9)
         self.ax_count = self.fig.add_axes([0.4, 0.8, 0.2, 0.2])
         self.count_template = 'Count: {:5d}'
         self.ax_count_form = self.ax_count.text(0.5, 0.5, self.count_template.format(0),
@@ -41,7 +45,9 @@ class Graphics:
                                            np.full(num_years, np.nan), 'b-')[0]
         self.line_carn = self.ax_line.plot(np.arange(num_years),
                                            np.full(num_years, np.nan), 'r-')[0]
-
+        if self.hist_specs is None:
+            self.hist_specs = {'fitness': {'max': 1.0, 'delta': 0.05}, 'age': {'max': 60.0, 'delta': 2},
+                               'weight': {'max': 60, 'delta': 2}}
 
     def counter(self, year):
 
@@ -63,13 +69,24 @@ class Graphics:
         self.fig.canvas.flush_events()
         plt.pause(1e-6)
 
+    def hist_plot(self, fitness_data, age_data, weight_data):
+        self.ax_hist3.clear()
+        self.ax_hist2.clear()
+        self.ax_hist1.clear()
+        self.ax_hist3.hist(fitness_data[0], bins=round(self.hist_specs['fitness']['max']/ self.hist_specs['fitness']['delta']), histtype=u'step')
+        self.ax_hist2.hist(age_data[0], bins=round(self.hist_specs['age']['max']/ self.hist_specs['age']['delta']), histtype=u'step')
+        self.ax_hist1.hist(weight_data[0], bins=round(self.hist_specs['age']['max']/ self.hist_specs['weight']['delta']), histtype=u'step')
+        plt.pause(1e-6)
+
+
+
     # def mapping(self):
     #
     #
     # def logger(self):
 
 
-if __name__ == '__main__':
-    grafikk = Graphics(ymax_animals=1, cmax_animals={'Herbivore': 50, 'Carnivore': 20})
-    grafikk.setup(100)
-    grafikk.line_plot()
+# if __name__ == '__main__':
+#     grafikk = Graphics(ymax_animals=1, cmax_animals={'Herbivore': 50, 'Carnivore': 20})
+#     grafikk.setup(100)
+#     grafikk.line_plot()
